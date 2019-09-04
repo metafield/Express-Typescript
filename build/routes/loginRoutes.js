50820f7ac5a311f0ef849836d415d986a9f46600
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2019-09-04 11:15:28 -0700

    adds improved interface for Request

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -8,5 +8,11 @@ router.get('/login', function (req, res) {
 });
 router.post('/login', function (req, res) {
     var _a = req.body, email = _a.email, password = _a.password;
-    res.send(email + password);
+    if (email) {
+        res.send(email.toLocaleUpperCase());
+    }
+    else {
+        res.statusCode = 422;
+        res.send('Please provide a valid email');
+    }
 });
