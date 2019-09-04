5270dc4d05915554500ebc9fbb5409de5442f0c6
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2019-09-04 09:40:24 -0700

    adds login routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import express, { Request, Response } from 'express'
-import { router } from './routes/loginRoutes'
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
 
-const app = express()
+const app = express();
 
-app.use(router)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
-const port = 3000
-
+const port = 3000;
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
