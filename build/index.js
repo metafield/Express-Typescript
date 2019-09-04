01884580c809b38813279d9a805f54a39c37bb8f
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2019-09-04 09:26:48 -0700

    installs and configures basic express server

--- build/index.js
+++ build/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("hi there");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+app.get("/", function (req, res) {
+    res.send("\n    <div>\n      <h1> Hi there! </h1>\n    </div>\n  ");
+});
+var port = 3000;
+app.listen(port, function () {
+    console.log("listening on port " + port);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hi there");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1> Hi there! </h1>
+    </div>
+  `);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
