a4ad79b55dcc8d6de76ab0cbeeb7358991215739
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2019-09-05 12:34:28 -0700

    refactors routes adds Enums for methods

--- build/controllers/decorators/routes.js
+++ build/controllers/decorators/routes.js
@@ -1,9 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
-function get(path) {
-    return function (target, key, desc) {
-        Reflect.defineMetadata('path', path, target, key);
+var Methods_1 = require("./Methods");
+function routeBinder(method) {
+    return function (path) {
+        return function (target, key, desc) {
+            Reflect.defineMetadata('path', path, target, key);
+            Reflect.defineMetadata('method', method, target, key);
+        };
     };
 }
-exports.get = get;
+exports.get = routeBinder(Methods_1.Methods.get);
+exports.put = routeBinder(Methods_1.Methods.put);
+exports.post = routeBinder(Methods_1.Methods.post);
+exports.del = routeBinder(Methods_1.Methods.del);
+exports.patch = routeBinder(Methods_1.Methods.patch);
