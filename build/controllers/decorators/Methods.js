4c72b9045b02d8b27ca4930cecd3b786b69bc9e1
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2019-09-05 13:18:49 -0700

    adds key enum fixes method enum

--- build/controllers/decorators/Methods.js
+++ build/controllers/decorators/Methods.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Methods;
 (function (Methods) {
-    Methods["get"] = "get";
-    Methods["post"] = "post";
-    Methods["patch"] = "patch";
-    Methods["del"] = "delete";
-    Methods["put"] = "put";
+    Methods["Get"] = "get";
+    Methods["Post"] = "post";
+    Methods["Patch"] = "patch";
+    Methods["Del"] = "delete";
+    Methods["Put"] = "put";
 })(Methods = exports.Methods || (exports.Methods = {}));
--- build/controllers/decorators/routes.js
+++ build/controllers/decorators/routes.js
@@ -2,16 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 var Methods_1 = require("./Methods");
+var MetadataKeys_1 = require("./MetadataKeys");
 function routeBinder(method) {
     return function (path) {
         return function (target, key, desc) {
-            Reflect.defineMetadata('path', path, target, key);
-            Reflect.defineMetadata('method', method, target, key);
+            Reflect.defineMetadata(MetadataKeys_1.MetadataKeys.Path, path, target, key);
+            Reflect.defineMetadata(MetadataKeys_1.MetadataKeys.Method, method, target, key);
         };
     };
 }
-exports.get = routeBinder(Methods_1.Methods.get);
-exports.put = routeBinder(Methods_1.Methods.put);
-exports.post = routeBinder(Methods_1.Methods.post);
-exports.del = routeBinder(Methods_1.Methods.del);
-exports.patch = routeBinder(Methods_1.Methods.patch);
+exports.get = routeBinder(Methods_1.Methods.Get);
+exports.put = routeBinder(Methods_1.Methods.Put);
+exports.post = routeBinder(Methods_1.Methods.Post);
+exports.del = routeBinder(Methods_1.Methods.Del);
+exports.patch = routeBinder(Methods_1.Methods.Patch);
