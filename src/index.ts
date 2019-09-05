0f9bf154062c431a279977f6153d7110614f3f8a
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2019-09-05 12:08:41 -0700

    refactors to have one router as singleton

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
-import { router as controllerRouter } from './controllers/decorators/controller';
 import './controllers/LoginController';
-const app = express();
+import { AppRouter } from './AppRouter';
 
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['dkjhdsff'] }));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 const port = 3000;
 app.listen(port, () => {
