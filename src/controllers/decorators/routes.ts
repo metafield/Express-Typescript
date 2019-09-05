4267d6282ccffbeb152d4f41424015cc6bc0578d
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2019-09-05 14:40:38 -0700

    handles type checking using prop descriptors

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,10 +1,15 @@
 import 'reflect-metadata';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
+import { RequestHandler } from 'express';
+
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
 
 function routeBinder(method: string) {
   return function(path: string) {
-    return function(target: any, key: string, desc: PropertyDescriptor) {
+    return function(target: any, key: string, desc: RouteHandlerDescriptor) {
       Reflect.defineMetadata(MetadataKeys.Path, path, target, key);
       Reflect.defineMetadata(MetadataKeys.Method, method, target, key);
     };
